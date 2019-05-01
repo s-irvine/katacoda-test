88f0fd58a4d1f2a079fd4b689c05c6f11e67a556
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-01 11:46:09 +0100

    add extra preload images

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 docker pull docker.io/ibmcom/portieris:0.5.1
+docker pull goharbor/harbor-adminserver:dev
 docker pull goharbor/clair-photon:dev
 docker pull goharbor/harbor-core:dev
 docker pull goharbor/harbor-jobservice:dev
@@ -9,5 +10,6 @@ docker pull goharbor/notary-server-photon:dev
 docker pull goharbor/notary-signer-photon:dev
 docker pull goharbor/harbor-portal:dev
 docker pull goharbor/registry-photon:dev
+docker pull goharbor/harbor-registryctl:dev
 docker pull goharbor/harbor-db:dev
 docker pull goharbor/redis-photon:dev
