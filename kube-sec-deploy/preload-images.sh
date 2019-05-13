87248737f613e8102cd7c144a42c95cc05dc22b5
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 15:38:03 +0100

    tidy up install script

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -17,11 +17,7 @@ docker pull goharbor/redis-photon:dev
 
 # Update docker (so we have a version with `docker trust` commands)
 systemctl stop kubelet
-systemctl stop docker
-cp /lib/systemd/system/docker.service .
 apt-get remove -y docker docker-engine docker.io containerd runc &&
-rm /etc/systemd/system/docker.service /etc/systemd/system/docker.socket &&
-systemctl daemon-reload &&
 apt-get update &&
 apt-get install -y \
         apt-transport-https \
@@ -38,7 +34,7 @@ apt-get update &&
 mv /etc/docker/daemon.json /etc/docker/daemon-old.json &&
 mv daemon.json /etc/docker/daemon.json &&
 apt-get install -y docker-ce docker-ce-cli containerd.io &&
-echo "install ok" &&
+echo "Install Complete" &&
 mv /etc/docker/daemon-old.json /etc/docker/daemon.json &&
 # Remove '-H fd://' from the command invocation of the docker service as it conflicts with the `daemon.json`
 sed 's/\ \-H\ fd\:\/\///g' /lib/systemd/system/docker.service > /lib/systemd/system/docker-new.service &&
@@ -46,6 +42,7 @@ mv /lib/systemd/system/docker-new.service /lib/systemd/system/docker.service &&
 cat /etc/docker/daemon.json | jq '.["insecure-registries"] += ["127.0.0.1/8"]' > /etc/docker/daemon.json &&
 systemctl daemon-reload &&
 systemctl restart docker &&
-systemctl start kubelet
+systemctl start kubelet &&
+echo "Kubelet Restarted"
 
 # Pull down a Clair DB and push it into the container so we don't need to wait for it to update
