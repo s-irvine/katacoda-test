caf399f9d1b518e9e8c2e310d3823c51396458c0
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 15:45:15 +0100

    fix: jq to other file

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -39,7 +39,8 @@ mv /etc/docker/daemon-old.json /etc/docker/daemon.json &&
 # Remove '-H fd://' from the command invocation of the docker service as it conflicts with the `daemon.json`
 sed 's/\ \-H\ fd\:\/\///g' /lib/systemd/system/docker.service > /lib/systemd/system/docker-new.service &&
 mv /lib/systemd/system/docker-new.service /lib/systemd/system/docker.service &&
-cat /etc/docker/daemon.json | jq '.["insecure-registries"] += ["127.0.0.1/8"]' > /etc/docker/daemon.json &&
+cat /etc/docker/daemon.json | jq '.["insecure-registries"] += ["127.0.0.1/8"]' > /etc/docker/daemon-new.json &&
+mv /etc/docker/daemon-new.json /etc/docker/daemon.json &&
 systemctl daemon-reload &&
 systemctl restart docker &&
 systemctl start kubelet &&
