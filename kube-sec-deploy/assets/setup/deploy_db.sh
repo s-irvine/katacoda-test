f5a6e10e80c407c797bdc078079abbdf4e1fdc17
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 16:31:17 +0100

    actually invoke psql in deploy script

--- kube-sec-deploy/assets/setup/deploy_db.sh
+++ kube-sec-deploy/assets/setup/deploy_db.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") -U postgres < clear.sql
-docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") -U postgres < vulnerability.sql
+docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < clear.sql
+docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < vulnerability.sql
