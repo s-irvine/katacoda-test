9ac08aa4e3b046453662c181329f52c43e7b00d0
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 17:18:57 +0100

    fix output of db script

--- kube-sec-deploy/assets/setup/deploy_db.sh
+++ kube-sec-deploy/assets/setup/deploy_db.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < clear.sql
-docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < vulnerability.sql
+docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < clear.sql > /dev/null
+echo "Deployed First DB"
+docker exec -i $(docker ps -qf "label=io.kubernetes.pod.name=harbor-harbor-database-0" -f "label=io.kubernetes.container.name=database") psql -U postgres < vulnerability.sql > /dev/null
+echo "Deployed Second DB"
+echo "Done"
