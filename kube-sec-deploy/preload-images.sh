8ff59c2155918465fa2ff276ad64a4d5ba1ae3dd
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 15:07:29 +0100

    fix comment

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -33,7 +33,7 @@ add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable" &&
-apt-get update #&&
+apt-get update &&
 mv /etc/docker/daemon.json /etc/docker/daemon-old.json &&
 mv daemon.json /etc/docker/daemon.json &&
 apt-get install -y docker-ce docker-ce-cli containerd.io  || true &&
