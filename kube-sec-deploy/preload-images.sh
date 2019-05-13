6e7b76523acbd0b606270132c16689ca72a6d45e
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 12:10:13 +0100

    fix: actually allow command to fail

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -31,7 +31,7 @@ add-apt-repository \
     $(lsb_release -cs) \
     stable" &&
 apt-get update &&
-apt-get install -y docker-ce docker-ce-cli containerd.io || true &&
+apt-get install -y docker-ce docker-ce-cli containerd.io;
 # Remove '-H fd://' from the command invocation of the docker service as it conflicts with the `daemon.json`
 sed 's/\ \-H\ fd\:\/\///g' /lib/systemd/system/docker.service > /lib/systemd/system/docker.service &&
 cat /etc/docker/daemon.json | jq '.["insecure-registries"] += ["127.0.0.1/8"]' > /etc/docker/daemon.json &&
