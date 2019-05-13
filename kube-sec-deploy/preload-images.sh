37245a9f291a02bfe1ec6276c38e5bdd6eb72b5c
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-13 11:29:50 +0100

    feat: add '&&' so 'apt-get' doesn't eat my input buffer

--- kube-sec-deploy/preload-images.sh
+++ kube-sec-deploy/preload-images.sh
@@ -17,21 +17,21 @@ docker pull goharbor/redis-photon:dev
 
 # Update docker (so we have a version with `docker trust` commands)
 systemctl stop kubelet
-apt-get remove -y docker docker-engine docker.io containerd runc
-apt-get update
+apt-get remove -y docker docker-engine docker.io containerd runc &&
+apt-get update &&
 apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
         gnupg-agent \
-        software-properties-common
+        software-properties-common &&
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
-    stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+    stable" &&
+apt-get update &&
+apt-get install -y docker-ce docker-ce-cli containerd.io &&
 # Remove '-H fd://' from the command invocation of the docker service as it conflicts with the `daemon.json`
 sed 's/\ \-H\ fd\:\/\///g' /lib/systemd/system/docker.service > /lib/systemd/system/docker.service
 systemctl start docker
