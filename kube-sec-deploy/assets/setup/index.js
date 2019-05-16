6b96701b9e443a79bdb3e89be384472d9f11a1dc
Sam Irvine <s-irvine@users.noreply.github.com>
2019-05-16 11:27:09 +0100

    downgrade demo-api

--- kube-sec-deploy/assets/setup/index.js
+++ kube-sec-deploy/assets/setup/index.js
@@ -1,13 +1,12 @@
 var ip = require('ip');
 var restify = require('restify');
-var pkg = require('./package.json');
 
 var server = restify.createServer({ name: 'demo' });
 
 var message = process.env.MESSAGE || 'Hello, world';
 
 server.get('/', function (req, res, next) {
-  res.send(200, `${message} | ${ip.address()} | Version ${pkg.version}`);
+  res.send(200, message + ' ' + ip.address() + '!');
   return next();
 });
 
